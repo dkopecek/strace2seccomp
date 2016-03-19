efbe647cbdb5732541583d896934d96f1d3a7d48
Daniel Kopeček <dkopecek@redhat.com>
2016-03-19 13:44:22 +0100

    Extended Syscall class

--- src/Syscall.cpp
+++ src/Syscall.cpp
@@ -8,14 +8,49 @@ namespace st2se
     value = _value;
   }
 
+  std::string Syscall::Argument::toString() const
+  {
+    switch(type) {
+      case ArgumentType::CONSTANT:
+        return value;
+      case ArgumentType::INTEGER:
+        return value;
+      case ArgumentType::STRING:
+        return std::string("string");
+      case ArgumentType::POINTER:
+        return std::string("pointer");
+      case ArgumentType::ARRAY:
+        return std::string("array");
+      case ArgumentType::STRUCTURE:
+        return std::string("struct");
+      case ArgumentType::UNKNOWN:
+        return std::string("<unknown>");
+    }
+  }
+
+  Syscall::Invocation::Invocation()
+  {
+    setComplete(false);
+  }
+
   void Syscall::Invocation::addArgument(const Argument& argument)
   {
     arguments.push_back(argument);
   }
 
+  void Syscall::Invocation::setComplete(bool _complete)
+  {
+    complete = _complete;
+  }
+
   Syscall::Syscall(const std::string& name)
   {
-    _name = name;
+    setName(name);
+  }
+
+  Syscall::operator bool() const
+  {
+    return !_name.empty();
   }
 
   const std::string& Syscall::name() const
@@ -46,6 +81,33 @@ namespace st2se
   Syscall::Invocation& Syscall::lastInvocation()
   {
     return _invocations.at(_invocations.size());
+  }
+
+  const std::vector<Syscall::Invocation>& Syscall::invocations() const
+  {
+    return _invocations;
+  }
+
+  void Syscall::write(std::ostream& stream) const
+  {
+    for (const auto& invocation : _invocations) {
+      stream << _name << "(";
+      for (const auto& argument : invocation.arguments) {
+        stream << argument.toString();
+        stream << " ";
+      }
+      if (invocation.complete) {
+        stream << ")";
+      }
+      else {
+        stream << "...";
+      }
+      stream << std::endl;
+    }
+  }
+
+  void Syscall::merge(const Syscall& syscall)
+  {
   }
 } /* namespace st2se */
 
--- src/Syscall.hpp
+++ src/Syscall.hpp
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <string>
+#include <ostream>
 
 namespace st2se
 {
@@ -22,6 +23,7 @@ namespace st2se
       struct Argument
       {
         Argument(ArgumentType _type = ArgumentType::UNKNOWN, const std::string& _value = "");
+        std::string toString() const;
 
         ArgumentType type;
         std::string value;
@@ -29,12 +31,17 @@ namespace st2se
 
       struct Invocation
       {
+        Invocation();
         void addArgument(const Argument& argument);
+        void setComplete(bool _complete = true);
 
         std::vector<Argument> arguments;
+        bool complete;
       };
 
       Syscall(const std::string& name = "");
+
+      operator bool() const;
 
       const std::string& name() const;
       void setName(const std::string& name);
@@ -43,7 +50,11 @@ namespace st2se
       void addInvocation(const Invocation& invocation);
       const Invocation& lastInvocation() const;
       Invocation& lastInvocation();
- 
+      const std::vector<Invocation>& invocations() const;
+
+      void write(std::ostream& stream) const;
+      void merge(const Syscall& syscall);
+
     private:
       std::string _name;
       std::vector<Invocation> _invocations;
