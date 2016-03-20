bd17a7c9827b31b5dbee0e2d282db7f6310a2d8e
Daniel Kopeček <dkopecek@redhat.com>
2016-03-20 20:00:15 +0100

    Initial seccomp code comman-line options

--- src/analyze.cpp
+++ src/analyze.cpp
@@ -2,11 +2,13 @@
 #include <string>
 #include <cstdlib>
 #include <fstream>
+#include <utility>
 #include <getopt.h>
 
 #include "analyze.hpp"
 #include "grammar.hpp"
 #include "DataSet.hpp"
+#include "SeccompCode.hpp"
 
 #ifndef NDEBUG
 #include <pegtl/analyze.hh>
@@ -14,9 +16,12 @@
 
 #include <pegtl/trace.hh>
 
-static const char *options_short = "t";
+static const char *options_short = "tls:o:";
 static const struct ::option options_long[] = {
   { "trace", no_argument, nullptr, 't' },
+  { "syscall-list", no_argument, nullptr, 'l' },
+  { "seccomp-code", required_argument, nullptr, 's' },
+  { "output-file", required_argument, nullptr, 'o' },
   { nullptr, 0, nullptr, 0 }
 };
 
@@ -26,14 +31,70 @@ int analyze(int argc, char *argv[], const std::string& datadir)
 {
   bool trace = false;
   int opt;
+  bool syscall_list = false;
+  bool seccomp_code = true;
+  bool seccomp_code_match_integers = false;
+  bool seccomp_code_match_constants = false;
+  std::pair<int,int> seccomp_code_integer_range(-1, 1024);
+  bool seccomp_code_integer_range_matching = false;
+  std::string output_file;
 
   while ((opt = getopt_long(argc, argv, options_short, options_long, nullptr)) != -1) {
     switch(opt) {
       case 't':
         trace = true;
         break;
+      case 'l':
+        syscall_list = true;
+        seccomp_code = false;
+        break;
+      case 's':
+        seccomp_code = true;
+        syscall_list = false;
+        {
+          char * const token[] = {
+            const_cast<char*>("int"),
+            const_cast<char*>("const"),
+            const_cast<char*>("name"),
+            const_cast<char*>("int-match-range"),
+            nullptr
+          };
+
+          char *subopts = optarg;
+          int not_found = 0;
+          char *value = nullptr;
+
+          while (*subopts != '\0' && !not_found) {
+            switch(getsubopt(&subopts, token, &value)) {
+              case 0: /* int */
+                seccomp_code_match_integers = true;
+                if (value) {
+                  int min, max;
+                  if (std::sscanf(value, "%d:%d", &min, &max) != 2) {
+                    std::cerr << "Invalid \"int\" suboption syntax. Correct syntax is <int>:<int>." << std::endl;
+                    return EXIT_FAILURE;
+                  }
+                  seccomp_code_integer_range = std::pair<int,int>(min, max);
+                }
+                break;
+              case 1: /* const */
+                seccomp_code_match_constants = true;
+                break;
+              case 2: /* name */
+                break;
+              case 3: /* int-match-range */
+                seccomp_code_integer_range_matching = true;
+                break;
+              default:
+                std::cerr << "Unknown --seccomp-code subtoption." << std::endl;
+                return EXIT_FAILURE;
+            }
+          }
+        }
+        break;
+      case 'o':
+        output_file = std::string(optarg);
       default:
-        std::cerr << "Unknown option" << std::endl;
         return EXIT_FAILURE;
     }
   }
@@ -61,7 +122,31 @@ int analyze(int argc, char *argv[], const std::string& datadir)
     }
   }
 
-  dataset.write(std::cout);
+  if (syscall_list) {
+    if (output_file.empty()) {
+      dataset.write(std::cout);
+    }
+    else {
+      std::ofstream stream(output_file, std::ios::trunc);
+      dataset.write(stream);
+    }
+  }
+  else if (seccomp_code) {
+    st2se::SeccompCodeParameters parameters = {
+      .match_constants = seccomp_code_match_constants,
+      .match_integers = seccomp_code_match_integers,
+      .integer_range = seccomp_code_integer_range,
+      .integer_range_matching = seccomp_code_integer_range_matching
+    };
+
+    if (output_file.empty()) {
+      st2se::generateSeccompCode(std::cout, dataset, parameters);
+    }
+    else {
+      std::ofstream stream(output_file, std::ios::trunc);
+      st2se::generateSeccompCode(stream, dataset, parameters);
+    }
+  }
 
   return EXIT_SUCCESS;
 }
